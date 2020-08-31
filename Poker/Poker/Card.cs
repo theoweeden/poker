58eee1ef28c706a713dafb75e2efb532ad552f52
Theo Weeden <theo.weeden@gmail.com>
2020-08-31 14:38:10 +0100

    Added a card class

---
+++ Poker/Poker/Card.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Poker
+{
+    class Card
+    {
+        public Number Number { get; set; }
+        public Suit Suit { get; set; }
+        public Card(Number number, Suit suit)
+        {
+            Number = number;
+            Suit = suit;
+        }
+    }
+
+    public enum Number
+    {
+        Ace = 1,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King,
+    }
+
+    public enum Suit
+    {
+        Clubs,
+        Diamonds,
+        Hearts,
+        Spades
+    }
+}
