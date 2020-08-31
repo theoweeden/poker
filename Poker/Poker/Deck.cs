43ad65d75783be4b9983c36ac3179502edb8e05d
Theo Weeden <theo.weeden@gmail.com>
2020-08-31 15:34:28 +0100

    Added methods to deal hands

--- Poker/Poker/Deck.cs
+++ Poker/Poker/Deck.cs
@@ -27,5 +27,27 @@ namespace Poker
             Cards.Clear();
             foreach (var c in tempCards.OrderBy(x => r.Next())) Cards.Push(c);
         }
+
+        public List<Card> DealHand(int size)
+        {
+            var hand = new List<Card>();
+            for (int i = 0; i < size; i++)
+            {
+                hand.Add(Cards.Pop());
+            }
+            return hand;
+        }
+
+        public List<List<Card>> DealHands(int number, int size)
+        {
+            var hands = new List<List<Card>>();
+
+            for (int i = 0; i < number; i++)
+            {
+                hands.Add(DealHand(size));
+            }
+
+            return hands;
+        }
     }
 }
