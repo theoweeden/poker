79fd25765731423f7913c4857b2e0fbce40ff9f8
Theo Weeden <theo.weeden@gmail.com>
2020-09-01 10:01:02 +0100

    Started wincheck implementation

--- Poker/Poker/WinChecker.cs
+++ Poker/Poker/WinChecker.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace Poker
 {
@@ -16,7 +17,66 @@ namespace Poker
 
         public (List<Card> Hand, WinType WinType) WinCheck(List<Card> table, List<List<Card>> hands)
         {
-            throw new NotImplementedException();
+            foreach(var hand in hands)
+            {
+                var pool = new List<Card>();
+                pool.AddRange(table);
+                pool.AddRange(hand);
+
+                //combos test
+                var combos = pool.GroupBy(x => x.Number).Select(x => (x.Key, count: x.Count())).OrderBy(x=>x.count).ThenBy(x => x.Key);
+                WinType highestCombo = ComboCheck(pool);
+
+                //suits test
+                var suits = pool.GroupBy(x => x.Suit).Select(x => (x.Key, count: x.Count())).OrderBy(x => x.count).ThenBy(x => x.Key);
+                WinType highestSuits;
+
+                if (suits.Any(x => x.count == 5)) highestCombo = WinType.Flush;
+
+                //straight test
+            }
+
+            return (null, WinType.HighCard);
+        }
+
+        private WinType ComboCheck(List<Card> pool)
+        {
+            var combos = pool.GroupBy(x => x.Number).Select(x => (x.Key, count: x.Count())).OrderBy(x => x.count).ThenBy(x => x.Key);
+
+            if (combos.Any(x => x.count == 4)) return WinType.FourOfAKind;
+            else if (combos.Any(x => x.count == 3) && combos.Any(x => x.count == 2)) return WinType.FullHouse;
+            else if (combos.Any(x => x.count == 3)) return WinType.ThreeOfAKind;
+            else if (combos.Count(x => x.count == 2) >= 2) return WinType.TwoPair;
+            else if (combos.Any(x => x.count == 2)) return WinType.Pair;
+
+            return WinType.HighCard;
+        }
+
+        private bool StraightCheck(List<Card> pool)
+        {
+            var orderedPool = pool.OrderBy(x => x.Number).ToList();
+            int counter = 1;
+            Number last = pool.First().Number;
+            for (int i = 1; i < orderedPool.Count; i++)
+            {
+                var diff = orderedPool[i].Number - last;
+                if (diff == 0) continue;
+                else if (diff == 1)
+                {
+                    counter++;
+                }
+                else if (diff != 1)
+                {
+                    counter = 1;
+                }
+                last = orderedPool[i].Number;
+            }
+
+            if (counter >= 5) return true;
+
+            if (counter >= 4 && orderedPool.Last().Number == Number.King && orderedPool.Any(x => x.Number == Number.Ace)) return true;
+
+            return false;
         }
     }
 
