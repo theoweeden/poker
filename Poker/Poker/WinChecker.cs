3de6890ef6679ffeca14b6ea478fb18637194591
Theo Weeden <theo.weeden@gmail.com>
2020-09-01 09:15:22 +0100

    Added functions to the win check class

--- Poker/Poker/WinChecker.cs
+++ Poker/Poker/WinChecker.cs
@@ -6,7 +6,18 @@ namespace Poker
 {
     class WinChecker
     {
+        public bool WinCheck(List<Card> table, List<Card> playerHand, List<List<Card>> otherHands = null)
+        {
+            var hands = new List<List<Card>>();
+            hands.AddRange(otherHands);
+            hands.Add(playerHand);
+            return WinCheck(table, hands).Hand == playerHand;
+        }
 
+        public (List<Card> Hand, WinType WinType) WinCheck(List<Card> table, List<List<Card>> hands)
+        {
+            throw new NotImplementedException();
+        }
     }
 
     public enum WinType
