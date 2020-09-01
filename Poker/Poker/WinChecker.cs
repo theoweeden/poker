c93789d086c86799941847af182a3527a2add527
Theo Weeden <theo.weeden@gmail.com>
2020-09-01 09:03:56 +0100

    Added WinChecker class

---
+++ Poker/Poker/WinChecker.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Poker
+{
+    class WinChecker
+    {
+
+    }
+
+    public enum WinType
+    {
+        RoyalFlush,
+        StraightFlush,
+        FourOfAKind,
+        FullHouse,
+        Flush,
+        Straight,
+        ThreeOfAKind,
+        TwoPair,
+        Pair,
+        HighCard
+    }
+}
