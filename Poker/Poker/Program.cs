6220113c0450eb3d5257da04e00061202667be98
Theo Weeden <theo.weeden@gmail.com>
2020-09-13 22:56:06 +0100

    Added test method

--- Poker/Poker/Program.cs
+++ Poker/Poker/Program.cs
@@ -17,5 +17,43 @@ namespace Poker
 
             Console.WriteLine(p);
         }
+
+        static void TestHand()
+        {
+            var table2 = new List<Card>()
+            {
+                new Card(Number.Ace, Suit.Diamonds),
+                new Card(Number.Two, Suit.Diamonds),
+                new Card(Number.Queen, Suit.Diamonds),
+                new Card(Number.Three, Suit.Diamonds),
+                new Card(Number.Five, Suit.Diamonds),
+            };
+
+            var hands2 = new List<List<Card>>()
+            {
+                new List<Card>()
+                {
+                    new Card(Number.Four, Suit.Diamonds),
+                    new Card(Number.Ten, Suit.Diamonds),
+                }
+            };
+
+            var winner2 = WinChecker.WinCheck(table2, hands2);
+
+            Console.WriteLine(winner2.WinType);
+            foreach (var h in winner2.Hands)
+            {
+                foreach (var c in h)
+                {
+                    Console.WriteLine(c.ToString());
+                }
+                Console.WriteLine();
+            }
+            foreach (var c in table2)
+            {
+                Console.WriteLine(c.ToString());
+            }
+            Console.WriteLine();
+        }
     }
 }
