d0aeab8cad34f14ca9b510f6474be7b2e1d84caf
Theo Weeden <theo.weeden@gmail.com>
2020-09-21 23:10:04 +0100

    removed test methods from program.cs

--- Poker/Poker/Program.cs
+++ Poker/Poker/Program.cs
@@ -20,47 +20,5 @@ namespace Poker
                 sw.WriteLine($"{h[0].ToString()}, {h[1].ToString()}, {r},");
             }
         }
-        static void TestHand()
-        {
-            var table = new List<Card>()
-            {
-                new Card(Rank.Ace, Suit.Diamonds),
-                new Card(Rank.Two, Suit.Diamonds),
-                new Card(Rank.Queen, Suit.Diamonds),
-                new Card(Rank.Three, Suit.Diamonds),
-                new Card(Rank.Five, Suit.Diamonds),
-            };
-
-            var hands = new List<List<Card>>()
-            {
-                new List<Card>()
-                {
-                    new Card(Rank.Four, Suit.Diamonds),
-                    new Card(Rank.Ten, Suit.Diamonds),
-                }
-            };
-
-            TestHand(table, hands);
-        }
-
-        static void TestHand(List<Card> Table, List<List<Card>> Hands)
-        {
-            var winner = WinChecker.WinCheck(Table, Hands);
-
-            Console.WriteLine(winner.WinType);
-            foreach (var h in winner.Hands)
-            {
-                foreach (var c in h)
-                {
-                    Console.WriteLine(c.ToString());
-                }
-                Console.WriteLine();
-            }
-            foreach (var c in Table)
-            {
-                Console.WriteLine(c.ToString());
-            }
-            Console.WriteLine();
-        }
     }
 }
