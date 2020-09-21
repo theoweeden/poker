ba8a7549830b8a91c1954e70c84f85e5defc7d4c
Theo Weeden <theo.weeden@gmail.com>
2020-09-21 23:53:38 +0100

    Added a new unit test class

---
+++ Poker/UnitTests/WinCheckerComprehensiveTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Poker;
+using System.Collections.Generic;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class WinCheckerComprehensiveTests
+    {
+        [TestMethod]
+        public void TestStraightCheck()
+        {
+
+        }
+    }
+}
