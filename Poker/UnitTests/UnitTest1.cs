5335258d344961a80e198053c34f8f82ae657cd6
Theo Weeden <theo.weeden@gmail.com>
2020-09-21 23:09:03 +0100

    Built some unit tests

--- Poker/UnitTests/UnitTest1.cs
+++ Poker/UnitTests/UnitTest1.cs
@@ -1,4 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Poker;
+using System.Collections.Generic;
 
 namespace UnitTests
 {
@@ -6,8 +8,53 @@ namespace UnitTests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestStraightFlush1()
         {
+            var table = new List<Card>()
+            {
+                new Card(Rank.Ace, Suit.Diamonds),
+                new Card(Rank.Two, Suit.Diamonds),
+                new Card(Rank.Queen, Suit.Diamonds),
+                new Card(Rank.Three, Suit.Diamonds),
+                new Card(Rank.Five, Suit.Diamonds),
+            };
+
+            var hands = new List<List<Card>>()
+            {
+                new List<Card>()
+                {
+                    new Card(Rank.Four, Suit.Diamonds),
+                    new Card(Rank.Ten, Suit.Diamonds),
+                }
+            };
+
+            var winner = WinChecker.WinCheck(table, hands);
+            Assert.AreEqual(WinType.StraightFlush, winner.WinType);
+        }
+
+        [TestMethod]
+        public void TestRoyalFlush()
+        {
+            var table = new List<Card>()
+            {
+                new Card(Rank.Ace, Suit.Diamonds),
+                new Card(Rank.King, Suit.Diamonds),
+                new Card(Rank.Queen, Suit.Diamonds),
+                new Card(Rank.Jack, Suit.Diamonds),
+                new Card(Rank.Five, Suit.Diamonds),
+            };
+
+            var hands = new List<List<Card>>()
+            {
+                new List<Card>()
+                {
+                    new Card(Rank.Four, Suit.Diamonds),
+                    new Card(Rank.Ten, Suit.Diamonds),
+                }
+            };
+
+            var winner = WinChecker.WinCheck(table, hands);
+            Assert.AreEqual(WinType.RoyalFlush, winner.WinType);
         }
     }
 }
